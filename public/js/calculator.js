a68750190374cb357599e87c3e8202d6b3f4a789
Mateusz Martin <mateusz.martinn@gmail.com>
2022-08-15 01:12:06 +0200

    Initial commit

---
+++ public/js/calculator.js
@@ -0,0 +1,26 @@
+/*let one = document.querySelector("#one");
+let two = document.querySelector("#two");
+let three = document.querySelector("#three");
+let four = document.querySelector("#four");
+let five = document.querySelector("#five");
+let six = document.querySelector("#six");
+let seven = document.querySelector("#seven");
+let eight = document.querySelector("#eight");
+let nine = document.querySelector("#nine");
+let zero = document.querySelector("#zero");
+let digit = document.querySelector("#digit");
+let plus = document.querySelector("#plus");
+let minus = document.querySelector("#minus");
+let multi = document.querySelector("#multi");
+let divi = document.querySelector("#divi");
+let equal = document.querySelector("#equal");
+let del = document.querySelector("#del");*/
+let input = document.querySelector("#input").value;
+let userInput = input.innerHTML;
+
+function example() {
+  console.log(input);
+}
+let t;
+
+t = setInterval(example, 1000);
